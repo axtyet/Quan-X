378c976fa505d552fcb206169c32769d0737210d
GitHub Action <action@github.com>
2024-03-01 18:40:26 +0800

    2024-03-01 18:40

--- chxm1023/Rewrite/Wall.js
+++ chxm1023/Rewrite/Wall.js
@@ -2,27 +2,40 @@
 
 项目名称：Wallcraft-动态壁纸
 下载地址：http://t.cn/A6iO7Eht
-使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+更新日期：2024-03-01
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
+^https?:\/\/billing-ios\.wallpaperscraft\.com\/(verify_receipt|products)\/remove_ads$ url script-response-body https://raw.githubusercontent.com/axtyet/Quan-X/main/chxm1023/Rewrite/Wall.js
 
-^https?:\/\/billing-ios\.wallpaperscraft\.com\/verify_receipt\/remove_ads$ url script-response-body https://raw.githubusercontent.com/axtyet/Quan-X/main/chxm1023/Rewrite/Wall.js
-
-[mitm] 
-
-hostname = *.wallpaperscraft.com
+[mitm]
+hostname = billing-ios.wallpaperscraft.com
 
 *************************************/
 
 
-var body = $response.body;
-var chxm1023 = JSON.parse(body);
+var chxm1023 = JSON.parse($response.body);
+const vipa = '/verify_receipt/remove_ads';
+const vipb = '/products/remove_ads';
 
-chxm1023.items["all_time"] = {
+if ($request.url.indexOf(vipa) != -1){
+  chxm1023.items["all_time"] = {
     "type" : "nonconsumable",
     "is_active" : true
-};
+  };
+  chxm1023.items["com.wallpaperscraft.wallpapers.year.1.5baks"] = {
+    "type" : "subscription",
+    "already_used_introductory_price" : false,
+    "is_active" : true
+  };
+}
+
+if ($request.url.indexOf(vipb) != -1){
+  chxm1023 = {  "items" : {   "nonconsumables" : [  "all_time" ],   "subscriptions" : [  "com.wallpaperscraft.wallpapers.year.1.5baks" ]}};
+}
 
 $done({body : JSON.stringify(chxm1023)});
