82b43471e80c8baf684f45c755cfb72a5bbc063e
GitHub Action <action@github.com>
2024-03-02 20:48:33 +0800

    2024-03-02 20:48

--- Guding88/Script/boshu.js
+++ Guding88/Script/boshu.js
@@ -3,10 +3,10 @@
 博树：https://apps.apple.com/app/id379968583
 
 [rewrite_local]
-^https?:\/\/api\.busuu\.com\/users\/me url script-response-body https://raw.githubusercontent.com/axtyet/Quan-X/main/Guding88/Script/boshu.js
+^https?:\/\/api\.(busuu|boshu)\.(com|cn)\/users\/me url script-response-body https://raw.githubusercontent.com/axtyet/Quan-X/main/Guding88/Script/boshu.js
 
 [MITM]
-hostname = api.busuu.com
+hostname = api.busuu.com, api.boshu.cn
 
 */
 var guding = JSON.parse($response.body);
